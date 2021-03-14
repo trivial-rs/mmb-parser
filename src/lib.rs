ac83f322c295690c3afeba89739eef30033d8fb9
Ivo C.S. Wingelaar <ivo@codeforge.rs>
2021-03-14 15:21:57 +0100

    Make `Mmb` struct fields private again
    These fields can now be accessed read-only with methods instead.

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,71 @@ pub use visitor::{ProofStream, UnifyStream, Visitor};
 
 #[derive(Debug)]
 pub struct Mmb<'a> {
-    pub file: &'a [u8],
-    pub version: u8,
-    pub num_sorts: u8,
-    pub num_terms: u32,
-    pub num_theorems: u32,
-    pub sorts: &'a [u8],
-    pub terms: &'a [u8],
-    pub theorems: &'a [u8],
-    pub proofs: &'a [u8],
-    pub index: Option<index::Index<'a>>,
+    file: &'a [u8],
+    version: u8,
+    num_sorts: u8,
+    num_terms: u32,
+    num_theorems: u32,
+    sorts: &'a [u8],
+    terms: &'a [u8],
+    theorems: &'a [u8],
+    proofs: &'a [u8],
+    index: Option<index::Index<'a>>,
 }
 
 impl<'a> Mmb<'a> {
     pub fn from(file: &'a [u8]) -> Option<Mmb<'a>> {
         parser::parse(file).map(|x| x.1).ok()
+    }
+
+    /// Return the slice containing the entire file
+    pub fn file(&self) -> &[u8] {
+        self.file
+    }
+
+    /// Return the version of the proof file format
+    pub fn version(&self) -> u8 {
+        self.version
+    }
+
+    /// Return the number of sorts in the sort table
+    pub fn num_sorts(&self) -> u8 {
+        self.num_sorts
+    }
+
+    /// Return the number of terms in the term table
+    pub fn num_terms(&self) -> u32 {
+        self.num_terms
+    }
+
+    /// Return the number of theorems in the theorem table
+    pub fn num_theorems(&self) -> u32 {
+        self.num_theorems
+    }
+
+    /// Return the slice containing the sort table
+    pub fn sorts(&self) -> &[u8] {
+        self.sorts
+    }
+
+    /// Return the slice containing the term table
+    pub fn terms(&self) -> &[u8] {
+        self.terms
+    }
+
+    /// Return the slice containing the theorem table
+    pub fn theorems(&self) -> &[u8] {
+        self.theorems
+    }
+
+    /// Return the slice containing the proof section
+    pub fn proofs(&self) -> &[u8] {
+        self.proofs
+    }
+
+    /// Return a reference to the optional index section
+    pub fn index(&self) -> Option<&index::Index> {
+        self.index.as_ref()
     }
 
     pub fn visit<V: Visitor<'a>>(
